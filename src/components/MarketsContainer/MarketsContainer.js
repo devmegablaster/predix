4cdf349048c4d2f1500fca9973fa834c3f03d2f4
Ojas Tyagi <devmegablaster@gmail.com>
2023-08-10 01:46:20 +0530

    feat: event card images

--- src/components/MarketsContainer/MarketsContainer.js
+++ src/components/MarketsContainer/MarketsContainer.js
@@ -197,6 +197,14 @@ export default function MarketsContainer() {
       console.log("some error occured");
     }
   };
+
+  const truncateString = (str, num) => {
+    if (str?.length <= num) {
+      return str;
+    }
+    return str?.slice(0, num) + "...";
+  };
+
   return (
     <>
       <div className="markets_main_header">
@@ -262,19 +270,19 @@ export default function MarketsContainer() {
                           <img src={TagPurple} alt="tag" />
                           {card?.category?.name}
                         </div>
-                        <img
-                          className="w-5"
-                          src={BookmarkEmpty}
-                          alt="Empty Bookmark"
-                        />
                       </div>
-                      <div className="markets_main_cardscontainer_card_content_top_left_namecontainer">
-                        {card?.marketDetails?.description}
+                      <div className="markets_main_cardscontainer_card_content_top_left_namecontainer max-w-[150px] 2xl:max-w-[280px]">
+                        {truncateString(card?.marketDetails?.description, 50)}
                       </div>
                     </div>
                     <div className="markets_main_cardscontainer_card_content_top_right">
                       <img
-                        className="w-full rounded-xl object-cover h-32"
+                        className="w-5 self-start mr-1 mt-2"
+                        src={BookmarkEmpty}
+                        alt="Empty Bookmark"
+                      />
+                      <img
+                        className="w-32 rounded-xl object-cover h-32"
                         src={card.marketDetails.imageURL !== "image" ? "https://ipfs.io/ipfs/" + card.marketDetails.imageURL : EventImage}
                         alt="eventImage"
                       />
